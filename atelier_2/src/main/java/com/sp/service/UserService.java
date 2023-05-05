d0736747ff72003c801147804faf294d1f5ef03f
ThomasGabriel2 <91309110+ThomasGabriel2@users.noreply.github.com>
2023-05-05 12:11:52 +0200

    Update UserService.java

--- atelier_2/src/main/java/com/sp/service/UserService.java
+++ atelier_2/src/main/java/com/sp/service/UserService.java
@@ -9,7 +9,6 @@ import com.sp.model.User;
 import com.sp.model.UserDTORegister;
 import com.sp.mapper.MapperUser;
 import com.sp.model.Card;
-import com.sp.model.CardTemplate;
 import com.sp.model.Hero;
 import com.sp.repository.UserRepository;
 
