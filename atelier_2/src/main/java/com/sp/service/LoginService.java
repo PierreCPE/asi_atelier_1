8293919fd43f4d695843c3eee4a099cfba887a14
Adrien Chausson <adrien.chausson@hotmail.fr>
2023-05-11 22:59:01 +0200

    login fonctionne

--- atelier_2/src/main/java/com/sp/service/LoginService.java
+++ atelier_2/src/main/java/com/sp/service/LoginService.java
@@ -5,6 +5,7 @@ import java.util.Optional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.sp.mapper.MapperUser;
 import com.sp.model.User;
 import com.sp.model.UserDTORegister;
 import com.sp.repository.UserRepository;
@@ -20,9 +21,12 @@ public class LoginService { // voir le site pour rendre en json le cookie
 //	}
 	
 	public int checklogin(UserDTORegister userdto) {
-		Optional<User> uOpt =uRepository.findByUserName(userdto.getUserName());
+		Optional<User> uOpt =uRepository.findBySurname(MapperUser.UserDTORegisterToUser(userdto).getSurname());
 		if (uOpt.isPresent()) {
+			System.out.println(userdto.getPassword());
+			System.out.println(uOpt.get().getPassword());
 			if (userdto.getPassword().equals(uOpt.get().getPassword())) {
+				System.out.println("rentre if");
 				return uOpt.get().getId();
 			}
 			return 0;
