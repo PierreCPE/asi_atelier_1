bfc5b5ca16c1daacee97910f272b9bc029d7bdb9
ThomasGabriel2 <91309110+ThomasGabriel2@users.noreply.github.com>
2023-05-12 10:37:46 +0200

    add MicroServiceUsers

---
+++ atelier_3/MicroServiceUsers/src/test/java/com/atelier3/MicroServices/MicroServiceUsers/MicroServiceUsersApplicationTests.java
@@ -0,0 +1,13 @@
+package com.atelier3.MicroServices.MicroServiceUsers;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MicroServiceUsersApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
