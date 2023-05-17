30e8be37a66e400241555da90f8f151e5a67453a
ThomasGabriel2 <91309110+ThomasGabriel2@users.noreply.github.com>
2023-05-17 16:14:12 +0200

    add AuthDTO

--- atelier_2/src/main/java/com/sp/service/UserService.java
+++ atelier_2/src/main/java/com/sp/service/UserService.java
@@ -28,9 +28,7 @@ public class UserService {
 	 * @return l'id de l'utilisateur
 	 */
 	public int addUser(UserDTORegister userDTO){
-		System.out.println(userDTO);
 		Optional<User> uOpt =uRepository.findByUserName(userDTO.getUserName());
-		System.out.println(uOpt);
 		if (!uOpt.isPresent()) {
 			User u = MapperUser.UserDTORegisterToUser(userDTO);
 			uRepository.save(u);
