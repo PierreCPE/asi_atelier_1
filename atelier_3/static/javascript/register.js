c95481a43da7ad233fc990beb54a25fe2008434f
Adrien Chausson <adrien.chausson@hotmail.fr>
2023-05-25 18:26:38 +0200

    config proxy

--- atelier_3/static/javascript/register.js
+++ atelier_3/static/javascript/register.js
@@ -1,30 +1,31 @@
 const form = document.getElementsByClassName('ui form')[0];
 form.addEventListener('submit', (event) => {
-    event.preventDefault()
+    event.preventDefault();
 
     let mdp1 = form.elements['mdp'].value;
     let mdp2 = form.elements['mdp2'].value;
-    
-    if (mdp1 == mdp2){
-	    let json = {};
-	    json['username'] = form.elements['name'].value;
-	    json['surname'] = form.elements['surname'].value;
-	    json['password'] = form.elements['mdp'].value;
-    }
-	else{
-		alert("Les 2 mots de passe ne sont pas identiques");
+
+    if (mdp1 == mdp2) {
+        let json = {};
+        json['username'] = form.elements['name'].value;
+        json['surname'] = form.elements['surname'].value;
+        json['password'] = form.elements['mdp'].value;
+
+        $.ajax({
+            url: "http://localhost:80/welcome/register",
+            contentType: "application/json",
+            dataType: 'json',
+            data: JSON.stringify(json),
+            type: 'POST',
+            complete: function(result) {
+                console.log(result);
+                if (result.responseJSON === true) {
+                    window.location.assign("http://localhost:80/welcome/login");
+                }
+            }
+        });
+    } else {
+        alert("Les 2 mots de passe ne sont pas identiques");
         return;
-	}
-	
-$.ajax({
-    url: "http://localhost:80/users/register",
-    contentType: "application/json",
-    dataType: 'json',
-    data: JSON.stringify(json),
-    type : 'POST',
-    complete: function(result){
-		console.log(result)
-		window.location.assign("http://localhost:80/login");
-	}})
-    
+    }
 });
